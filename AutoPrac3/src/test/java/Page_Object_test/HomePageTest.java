26830db1ba580d4179e677323d82797ccc0295d0
ahadsheraz96 <40655106+ahadsheraz96@users.noreply.github.com>
2020-01-28 02:13:21 -0500

    maven projct

--- AutoPrac3/src/test/java/Page_Object_test/HomePageTest.java
+++ AutoPrac3/src/test/java/Page_Object_test/HomePageTest.java
@@ -1,6 +1,7 @@
 package Page_Object_test;
 
 import base.SampleBrowserDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,10 +12,24 @@ public class HomePageTest extends SampleBrowserDriver {
     @BeforeMethod
     public void initElements(){
         obj1= PageFactory.initElements(driver,HomePage.class);
+
     }
     @Test
     public void searchTest(){
         obj1.search();
 
     }
+    @Test
+    public void testNumber2(){
+        obj1.viewCartTest();
+    }
+    @Test
+    public void test3(){
+        obj1.search();
+    }
+    @Test
+    public void testNUmber3(){
+        obj1.addingToCartTest();
+    }
+
 }
--- AutoPrac3/src/test/java/pageobject/HomePage.java
+++ AutoPrac3/src/test/java/pageobject/HomePage.java
@@ -1,11 +1,17 @@
 package pageobject;
 
 import base.SampleBrowserDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.testng.annotations.Test;
 
-public class HomePage  {
+public class HomePage extends SampleBrowserDriver {
+
+
+
     @FindBy(how = How.ID, using = "search_query_top")
     WebElement searchBox;
 
@@ -13,9 +19,45 @@ public class HomePage  {
     WebElement productcount;
 
 
-    public void search() {
+    public void search () {
         searchBox.sendKeys("women");
         searchBox.click();
+    }
+
+
+
+
+    public void searchTest(){
+        driver.findElement(By.id("search_query_top")).sendKeys("women");
+        driver.findElement(By.name("submit_search")).click();
 
     }
+    public void contactusTest(){
+        driver.findElement(By.linkText("Contact us")).click();
+        driver.findElement(By.id("email")).sendKeys("ahadsheraz96@.com");
+        driver.findElement(By.xpath("//input[@id='id_order']")).sendKeys("123456789");
+        // driver.findElement(By.xpath("//button[@id='submitMessage']")).click();
+    }
+    public void addingToCartTest(){
+        driver.findElement(By.linkText("Women")).click();
+        driver.findElement(By.xpath("//a[@class='button ajax_add_to_cart_button btn btn-default']")).click();
+        //driver.findElement(By.xpath("//span[@class='continue btn btn-default button exclusive-medium']")).click();
+
+    }
+    public void viewCartTest(){
+        driver.findElement(By.xpath("//b")).click();
+        driver.findElement(By.id("newsletter-input")).sendKeys("ahad123");
+    }
+    public void bestSellingTest(){
+        driver.findElement(By.linkText("Best sellers" )).click();
+        driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[2]")).click();
+        //driver.findElement(By.xpath("//div[@class='quick-view-wrapper-mobile']")).click();
+        //driver.findElement(By.linkText("Add to cart")).click();
+    }
+
+
+
+
+
+
 }
